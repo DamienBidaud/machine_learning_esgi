83cb3ea51c2f559ef1691d1b4ceb9f5c4bbdd742
dartian <bidauddamien6@gmail.com>
2016-04-28 16:27:21 +0200

    correctif

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ extern "C"{
 
     /**
      * créer le model
-     *//*
+     */
     __declspec(dllexport) void createModel(int nbInput, double* model){
         for(int i = 0; i < nbInput; i++){
             model[i] = 0;
         }
     }
 
-    *//**
+    /**
      * calcule le parceptron
      * val = couple de coordonée sur unity
      * model = c'est le model créer précedement
      * size = taille de l'élément
-     *//*
+     */
     __declspec(dllexport) double perceptron(double* value, double*  model, int size){
         double sum = 0;
         for(int i = 0; i < size; i++){
@@ -30,9 +30,9 @@ extern "C"{
     }
 
 
-    *//**
+    /**
      * prend un tableau 1 dimension et le transform en 2 dimension
-     *//*
+     */
      double** cleanArray(double* value, int size, int sizeElement){
         double** array = new double*[size/sizeElement];
         for(int i = 0; i < size/sizeElement; i++){
@@ -50,16 +50,16 @@ extern "C"{
         return array;
     }
 
-   *//* __declspec(dllexport) void deleteArray(double** array, int size){
+   /* __declspec(dllexport) void deleteArray(double** array, int size){
         for(int i = 0; i < size; i++){
             delete array[i];
         }
         delete array;
-    }*//*
+    }*/
 
-    *//**
+    /**
      * entraine le model
-     *//*
+     */
     __declspec(dllexport) void pal(double* exemplValue, int size,int inputSize, double* model, double* waitValue, double coef, int maxIter){
         int iter = 0;
         double** value = cleanArray(exemplValue, size, inputSize);
@@ -84,17 +84,4 @@ extern "C"{
         //deleteArray(value, size/inputSize);
     }
 
-    __declspec(dllexport) double lineaire_model(int w[], int x[], int sumLength){
-        int sum = 0;
-        for(int i = 0; i < sumLength; i++){
-            sum+= w[i] * x[i];
-        }
-        return sum;
-    }
-
-*/
-
-    __declspec(dllexport) double test(){
-        return 42.0;
-    }
 }
